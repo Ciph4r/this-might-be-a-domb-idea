602db0db8ed4c92aa63f42e3489e4715cbd394e4
David Lau <David.lau@codeimmersives.com>
2020-02-10 14:47:23 -0500

    gg

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const para = document.querySelector('p')
 
-para.style.color = 'lightBlue'
+para.style.color = 'red'
 para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
 
 const header = document.querySelector('h1')
 
 header.style.frontSize = '5em'
 
-const item13 = document.querySelector('#item-13').style.opacity = "0.5";
+const item13 = document.querySelector('#item-13')
 
+item13.style.opacity = "0.5";
 
 const item3 = document.querySelector('#item-3').innerText = 'I say, "Hi!"'
 const trex = document.querySelector('img')
@@ -23,10 +24,11 @@ myimage.height = '300'
 
 const list = document.querySelector('ul')
 
-//  const newLi = list.appendChild((document.createElement('li')(document.createTextNode())).setAttribute('id','item-16'))
+// const newLi = list.appendChild(document.createElement('li')).setAttribute('id','item-16')
  
 
-const newLi = list.appendChild(document.createElement('li')).innerText = `Won't get fooled again.`
+const newLi = list.appendChild(document.createElement('li'))
+newLi.innerText = `Won't get fooled again.`
 newLi.setAttribute('id','item-16')
 
 // newLi.innerText = `Won't get fooled again.`
