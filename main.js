24edce2936e9e21f340bdf656dc678205653b328
David Lau <David.lau@codeimmersives.com>
2020-02-10 14:40:28 -0500

    gg

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const para = document.querySelector('p')
 
-para.style.color = 'red'
+para.style.color = 'lightBlue'
 para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
 
 const header = document.querySelector('h1')
 
 header.style.frontSize = '5em'
 
-const item13 = document.querySelector('#item-13')
+const item13 = document.querySelector('#item-13').style.opacity = "0.5";
 
-item13.style.opacity = "0.5";
 
 const item3 = document.querySelector('#item-3').innerText = 'I say, "Hi!"'
 const trex = document.querySelector('img')
